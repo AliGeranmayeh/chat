6400fb36987f00df605bd89a5dd64a72bf3848c2
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-01-26 14:15:21 +0330

    update deleteconfig.php

--- chat/deleteconfog.php
+++ chat/deleteconfog.php
@@ -3,8 +3,8 @@ require_once "./../helper/helper.php";
 session_start();
 $user_message = explode(",,,",$_POST["delete_input"]);
 $data = json_decode(file_get_contents(STORAGE . "messages.json"), true);
-var_dump(explode("/",$user_message[2]));
 $directory_path=explode("/",$user_message[2]);
+var_dump($directory_path);
 foreach ($data as $key=>$item) {
     if ($item["username"]==$_SESSION["username"]&&$item["time"]==$user_message[1]&&$item["message"]==$user_message[0]&&$item["pic"]==$user_message[2]) {
         unset($data[$key]);
@@ -15,4 +15,3 @@ foreach ($data as $key=>$item) {
 }
 file_put_contents(STORAGE . "messages.json", json_encode($data, JSON_PRETTY_PRINT));
 view("chat");
-?>
